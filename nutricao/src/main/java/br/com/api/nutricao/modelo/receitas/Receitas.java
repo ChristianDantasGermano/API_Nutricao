fd0e5f828f65526a803211a470661d43330dc53b
Brendo Bubela Vidal <brendobubela@gmail.com>
2020-11-18 22:16:20 -0300

    Changes in classes "Receitas" and subclasses

--- nutricao/src/main/java/br/com/api/nutricao/modelo/receitas/Receitas.java
+++ nutricao/src/main/java/br/com/api/nutricao/modelo/receitas/Receitas.java
@@ -3,15 +3,27 @@ package br.com.api.nutricao.modelo.receitas;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import br.com.api.nutricao.modelo.alimentos.Alimentos;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@SuppressWarnings("serial")
+@Entity
 public class Receitas {
-	protected String nome;
-	private Map<Alimentos, Integer> ingredientes = new HashMap<Alimentos, Integer>();
-	protected String ComoPreparar;
-	protected double TempoPreparo;
-	protected String Observacao;	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter private long id;
+	
+	@Column
+	@Getter @Setter protected String nome;
+	@Getter @Setter private Map<Alimentos, Integer> ingredientes = new HashMap<Alimentos, Integer>();
+	@Getter @Setter protected String ComoPreparar;
+	@Getter @Setter protected double TempoPreparo;
+	@Getter @Setter protected String Observacao;	
 }
--- nutricao/src/main/java/br/com/api/nutricao/modelo/receitas/ReceitasEspeciais.java
+++ nutricao/src/main/java/br/com/api/nutricao/modelo/receitas/ReceitasEspeciais.java
@@ -1,11 +1,21 @@
 package br.com.api.nutricao.modelo.receitas;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor @AllArgsConstructor
+@SuppressWarnings("serial")
+@Entity
 public class ReceitasEspeciais extends Receitas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter private long id;
+	
+	@Column
 	@Getter @Setter protected String efeitoEspecial;
 }
