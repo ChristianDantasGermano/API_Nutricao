57f1facd8776ff3e6ed1dcf521c81b2322450bfb
Christian Dantas Germano <51031714+ChristianDantasGermano@users.noreply.github.com>
2020-11-21 12:55:09 -0300

    Updated Front

--- nutricao/src/main/java/br/com/api/nutricao/controle/ControleAlimentos.java
+++ nutricao/src/main/java/br/com/api/nutricao/controle/ControleAlimentos.java
@@ -9,12 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import br.com.api.nutricao.modelo.alimentos.Alimentos;
 import br.com.api.nutricao.modelo.alimentos.TabelaNutricional;
 import br.com.api.nutricao.modelo.container.AlimentosContainer;
-import br.com.api.nutricao.repository.AlimentosRepository;
 
 @Controller
 public class ControleAlimentos {
 	@Autowired
-	private AlimentosRepository alimentos;
+	private br.com.api.nutricao.repository.AlimentosRepository alimentos;
 	
 	@RequestMapping(value="/addalimento",method = RequestMethod.POST)
 	public String cadastrar(@ModelAttribute("alimentoForm") AlimentosContainer alimento,
@@ -24,10 +23,9 @@ public class ControleAlimentos {
 		System.out.println(alimento.getVitaminas());
 		nutri.setMinerios(alimento.getMinerios());
 		nutri.setVitaminas(alimento.getVitaminas());
-		alimenta.setTabelaNutricional(nutri);
+		alimenta.setTabelaNutricional(nutri);	
 		alimentos.save(alimenta);
 		return "redirect:/alimentos";
 	}
-	
 	
 }
--- nutricao/src/main/java/br/com/api/nutricao/controle/Routes.java
+++ nutricao/src/main/java/br/com/api/nutricao/controle/Routes.java
@@ -1,11 +1,21 @@
 package br.com.api.nutricao.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.api.nutricao.modelo.alimentos.Alimentos;
+import br.com.api.nutricao.repository.AlimentosRepository;
 
 @Controller
 public class Routes {
-
+	
+	@Autowired
+	private AlimentosRepository alimentos;
+	
 	//Página Home
 	@RequestMapping("/")
 	public String home() {
@@ -14,8 +24,25 @@ public class Routes {
 	
 	//
 	@RequestMapping("/alimentos")
-	public String Alimentos() {
-		return "alimentos";
+	public ModelAndView Alimentos() {
+		Iterable<Alimentos> elementos = alimentos.findAll();
+		Alimentos alimento = alimentos.findById(elementos.iterator().next().getId());;
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("alimentos");
+		mv.addObject("elementos",elementos);
+		mv.addObject("alimentos",alimento);
+		return mv;
+	}
+	
+	@RequestMapping(value="/alimentos/{id}",method = RequestMethod.GET)
+	public ModelAndView ConsultarNutricao(@PathVariable long id) {
+		Iterable<Alimentos> elementos = alimentos.findAll();
+		Alimentos alimento = alimentos.findById(id);;
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("alimentos");
+		mv.addObject("elementos",elementos);
+		mv.addObject("alimentos",alimento);
+		return mv;
 	}
 	
 }
